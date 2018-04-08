63e588d2338d24baca0e27a59d93629b610379a5
polymetric <polymetric@nodium.net>
2018-04-07 21:49:08 -0400

    make the program actually output a file instead of printing the gcode

--- src/Movespeed.java
+++ src/Movespeed.java
@@ -1,11 +1,16 @@
+import java.io.PrintWriter;
 
 public class Movespeed {
-	
 	final static String file = "test.gcode";
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		String output = "";
 		output += Processes.test_proc_1(Machine.PRUSA_I3_MK3);
-		System.out.println(output);
+		
+		// Create and write file
+		PrintWriter writer = new PrintWriter(file, "UTF-8");
+		writer.println(output);
+		writer.close();
+		System.out.println("wrote " + file);
 	}
 }
